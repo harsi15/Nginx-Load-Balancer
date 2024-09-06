34f9085e26411c6b0fe69b865f853c9d43930f06
Harsimran Singh Dhillon <65386992+harsi15@users.noreply.github.com>
2024-09-06 20:54:04 +0530

    Adding Webservers

---
+++ webserver1.js
@@ -0,0 +1,7 @@
+const http = require('http');
+const hostname = '127.0.0.1';
+const port = 1313;
+const server = http.createServer ((req, res) => { res.statusCode = 200; res.setHeader('Content-Type', 'text/plain'); res.end( 'Hello World from Server One');
+});
+server.listen(port, hostname, () => {
+console.log('Server running at http://${hostname}:${port}/'); });
